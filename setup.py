84c2089d5aab4438622d961f47f5957fe8f4cf1c
Maksymilian Ratajczyk <kontakt@maksymilianratajczyk,p>
2018-03-14 01:00:10 +0100

    setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='Simple Git',
+    version='0.1',
+    author='Maksymilian Ratajczyk',
+    author_email='kontakt@maksymilianratajczyk.pl',
+    description=('Simple implementation of git like system'
+                 'in Python'),
+    license='MIT',
+    py_modules=['sgit', 'simple_git', 'formats'],
+    url='https://github.com/mratajczyk/simple-git',
+    install_requires=required,
+    entry_points='''
+        [console_scripts]
+        sgit=sgit:cli
+    '''
+)
